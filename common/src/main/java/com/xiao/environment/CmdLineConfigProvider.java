9e575643f07fac08c9fa7243abc143f9a1ad8934
lix-wang <lix.wang@alo7.com>
2019-04-29 10:41:29 +0800

    add environment values provider

---
+++ common/src/main/java/com/xiao/environment/CmdLineConfigProvider.java
@@ -0,0 +1,45 @@
+package com.xiao.environment;
+
+import com.xiao.config.Constants;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+/**
+ *
+ * @author lix wang
+ */
+@Slf4j
+@Configuration
+public class CmdLineConfigProvider {
+    @Value("${" + Constants.PROFILE_PROPERTY_NAME + "}")
+    private String profile = "default,dev";
+
+    @Value("${server.port}")
+    private int port = Constants.DEFAULT_PORT;
+
+    @Value("${com.server-name}")
+    private String serverName = "unspecified";
+
+    @Bean
+    public CmdLineConfig initCmdLineConfig() {
+        CmdLineConfig cmdLineConfig = new CmdLineConfig();
+        cmdLineConfig.setProfile(ProfileType.getProfile(profile));
+        cmdLineConfig.setHostName(getHostName());
+        cmdLineConfig.setServerName(serverName);
+        return cmdLineConfig;
+    }
+
+    private String getHostName() {
+        try {
+            return InetAddress.getLocalHost().getHostName();
+        } catch (UnknownHostException e) {
+            log.warn("Get hostName failed. ", e);
+            return "localhost";
+        }
+    }
+}
---
+++ springboot-demo/src/main/java/com/xiao/SpringDemoServer.java
@@ -0,0 +1,15 @@
+package com.xiao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ * @author lix wang
+ */
+@SpringBootApplication
+public class SpringDemoServer {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDemoServer.class);
+    }
+}
