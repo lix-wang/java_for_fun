c9b8452c755daa769d71fc6e55bbcdf3cacb169d
lix-wang <lix.wang@alo7.com>
2019-05-07 18:20:51 +0800

    add cutomize logging

--- springboot-demo/src/test/java/com/xiao/service/LogServiceTest.java
+++ springboot-demo/src/test/java/com/xiao/service/LogServiceTest.java
@@ -1,8 +1,8 @@
 package com.xiao.service;
 
 import com.xiao.SpringDemoServer;
-import com.xiao.log.LoggerFactoryService;
-import com.xiao.log.LoggerTypeEnum;
+import com.xiao.logging.LoggerFactoryService;
+import com.xiao.logging.LoggerTypeEnum;
 import org.apache.logging.log4j.Logger;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -22,11 +22,11 @@ public class LogServiceTest {
     @Test
     public void testPrintLog() {
         Logger log = loggerFactoryService.getLogger(LoggerTypeEnum.DEFAULT_LOGGER);
-        log.trace("I am a trace log");
-        log.debug("I am a debug log");
-        log.info("I am a info log");
-        log.warn("I am a warn log");
-        log.error("I am a error log");
-        log.fatal("I am a fatal log");
+        log.trace("I am a trace logging");
+        log.debug("I am a debug logging");
+        log.info("I am a info logging");
+        log.warn("I am a warn logging");
+        log.error("I am a error logging");
+        log.fatal("I am a fatal logging");
     }
 }
