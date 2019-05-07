a94f22521bf7316cfe01b80dfb4c8a7c91a0eaaa
lix-wang <lix.wang@alo7.com>
2019-05-07 18:12:03 +0800

    add custom logs

--- common/src/main/java/com/xiao/environment/LixConfiguration.java
+++ common/src/main/java/com/xiao/environment/LixConfiguration.java
@@ -8,11 +8,12 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
+ * Means current annotated bean is used to config values.
  *
  * @author lix wang
  */
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 @Configuration
-public @interface EnvConfiguration {
+public @interface LixConfiguration {
 }
