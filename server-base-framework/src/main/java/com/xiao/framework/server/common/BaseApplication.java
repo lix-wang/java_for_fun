aa026c15a37ed66e27deeae04e83d52fb99a0e52
lix-wang <lix.wang@alo7.com>
2019-05-24 16:43:49 +0800

    add exclude auto configurations

--- server-base-framework/src/main/java/com/xiao/framework/server/common/BaseApplication.java
+++ server-base-framework/src/main/java/com/xiao/framework/server/common/BaseApplication.java
@@ -2,15 +2,36 @@ package com.xiao.framework.server.common;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.admin.SpringApplicationAdminJmxAutoConfiguration;
+import org.springframework.boot.autoconfigure.cache.CacheAutoConfiguration;
+import org.springframework.boot.autoconfigure.dao.PersistenceExceptionTranslationAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.JdbcTemplateAutoConfiguration;
+import org.springframework.boot.autoconfigure.jmx.JmxAutoConfiguration;
+import org.springframework.boot.autoconfigure.mail.MailSenderValidatorAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.client.RestTemplateAutoConfiguration;
+import org.springframework.boot.autoconfigure.websocket.reactive.WebSocketReactiveAutoConfiguration;
+import org.springframework.boot.autoconfigure.websocket.servlet.WebSocketServletAutoConfiguration;
 import org.springframework.context.ConfigurableApplicationContext;
 
 /**
+ * Temporarily exclude AutoConfigurations that not be used yet.
  *
  * @author lix wang
  */
 @SpringBootApplication(exclude = {
-        DataSourceAutoConfiguration.class
+        SpringApplicationAdminJmxAutoConfiguration.class,
+        CacheAutoConfiguration.class,
+        PersistenceExceptionTranslationAutoConfiguration.class,
+        JdbcTemplateAutoConfiguration.class,
+        DataSourceTransactionManagerAutoConfiguration.class,
+        DataSourceAutoConfiguration.class,
+        JmxAutoConfiguration.class,
+        MailSenderValidatorAutoConfiguration.class,
+        RestTemplateAutoConfiguration.class,
+        WebSocketReactiveAutoConfiguration.class,
+        WebSocketServletAutoConfiguration.class
 })
 public abstract class BaseApplication {
     protected static ConfigurableApplicationContext run(Class<?> primarySource, String[] args) {
