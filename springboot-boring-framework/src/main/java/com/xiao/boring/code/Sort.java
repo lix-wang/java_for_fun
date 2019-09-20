d8158b0e1e5baeac295d2af592a8331164097d2c
lix-wang <lix.wang@alo7.com>
2019-09-20 19:38:57 +0800

    add some boring code

---
+++ springboot-boring-framework/src/main/java/com/xiao/boring/code/Sort.java
@@ -0,0 +1,49 @@
+package com.xiao.boring.code;
+
+/**
+ * @author lix wang
+ */
+public class Sort {
+    /**
+     * 快排，使用左右探测方式。
+     */
+    public static int[] quickSort(int[] source) {
+        quickSort(source, 0, source.length - 1);
+        return source;
+    }
+
+    private static void quickSort(int[] source, int startPos, int endPos) {
+        if (startPos > endPos) {
+            return;
+        }
+        int i = startPos;
+        int j = endPos;
+        int temp = source[startPos];
+        while (i < j) {
+            while (source[j] >= temp && j > i) {
+                j--;
+            }
+            if (i < j) {
+                source[i] = source[j];
+                i++;
+            }
+            while (source[i] <= temp && i < j) {
+                i++;
+            }
+            if (i < j) {
+                source[j] = source[i];
+                j--;
+            }
+        }
+        source[i] = temp;
+        quickSort(source, startPos, i - 1);
+        quickSort(source, j + 1, endPos);
+    }
+
+    public static void main(String[] args) {
+        int[] result = Sort.quickSort(new int[] {1, 2, 4, 3, 7, 9, 8, 5, 6});
+        for (int element : result) {
+            System.out.println(element + " ");
+        }
+    }
+}
