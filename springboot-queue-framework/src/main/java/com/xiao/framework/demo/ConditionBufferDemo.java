15add30942687d0ef22ace787546e9e61bfa7402
lix-wang <lix.wang@alo7.com>
2019-12-25 19:27:11 +0800

    add concurrency notes

---
+++ springboot-queue-framework/src/main/java/com/xiao/framework/demo/ConditionBufferDemo.java
@@ -0,0 +1,56 @@
+package com.xiao.framework.demo;
+
+import org.omg.CORBA.Object;
+
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ *
+ * @author lix wang
+ */
+public class ConditionBufferDemo<T> {
+    private final Lock lock = new ReentrantLock();
+    private final Condition notFull = lock.newCondition();
+    private final Condition notEmpty = lock.newCondition();
+
+    private final T[] items = (T[]) new Object[100];
+    private int tail, head, count;
+
+    public void put(T x) throws InterruptedException {
+        lock.lock();
+        try {
+            while (count == items.length) {
+                notFull.await();
+            }
+            items[tail] = x;
+            if (++tail == items.length) {
+                tail = 0;
+            }
+            ++count;
+            notEmpty.signal();
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public T take() throws InterruptedException {
+        lock.lock();
+        try {
+            while (count == 0) {
+                notEmpty.await();
+            }
+            T x = items[head];
+            items[head] = null;
+            if (++head == items.length) {
+                head = 0;
+            }
+            --count;
+            notFull.signal();
+            return x;
+        } finally {
+            lock.unlock();
+        }
+    }
+}
